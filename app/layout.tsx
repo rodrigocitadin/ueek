6d5b4c2fa1c70856fa00a570e8c9c1b46e50fd19
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-27 11:37:15 -0300

    nav bar basic structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={rubik.className}>{children}</body>
+      <body className={`${rubik.className} bg-background text-white`}>
+        <nav className='flex justify-between container m-auto py-4'>
+          <img src='/logo.svg' />
+          <ul className='flex'>
+            <li className='mx-5 my-auto'><a href='#'>Inicio</a></li>
+            <li className='mx-5 my-auto'><a href='#'>Como funciona?</a></li>
+            <li className='mx-5 my-auto'><a href='#'>Prêmio</a></li>
+            <li className='mx-5 my-auto bg-blue-light py-3 px-5 rounded-3xl'><a href='#'>Comprar número</a></li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }

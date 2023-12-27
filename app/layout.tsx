4420b397684e74b7c6b092bd622d81729d9ad9dc
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-27 15:57:42 -0300

    switch to new component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Rubik } from 'next/font/google'
 import './globals.css'
+import BuyNumberButton from './components/BuyNumberButton'
 
 const rubik = Rubik({
   subsets: ['latin'],
@@ -27,7 +28,9 @@ export default function RootLayout({
               <li className='mx-5 my-auto'><a href='#'>Inicio</a></li>
               <li className='mx-5 my-auto'><a href='#'>Como funciona?</a></li>
               <li className='mx-5 my-auto'><a href='#'>Prêmio</a></li>
-              <li className='mx-5 my-auto bg-blue-light py-3 px-5 rounded-3xl'><a href='#'>Comprar número</a></li>
+              <li className='mx-5 my-auto'><a href='#'>
+                <BuyNumberButton />
+              </a></li>
             </ul>
           </nav>
           {children}

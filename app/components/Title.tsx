ebfb735329e3a0ade728192d72fd65fa4524a884
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-29 09:37:55 -0300

    now title can accept className

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -1,10 +1,11 @@
-export default function Title({ children }:
+export default function Title({ className, children }:
   {
+    className?: string,
     children: React.ReactNode
   }
 ) {
   return (
-    <h2 className={"text-4xl font-normal"}>
+    <h2 className={`text-4xl font-normal ${className}`}>
       {children}
     </h2>
   )

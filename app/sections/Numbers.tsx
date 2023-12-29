b7335e6d535285e967d6e844d2af2e9508cd442f
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-29 10:01:03 -0300

    wip

--- app/sections/Numbers.tsx
+++ app/sections/Numbers.tsx
@@ -1,21 +1,29 @@
 import BuySelectedNumbersButton from "../components/BuySelectedNumbersButton";
 import CannotBuyButton from "../components/CannotBuyButton"
+import SmallText from "../components/SmallText";
 import Title from "../components/Title"
 
 const numbers: number[] = [1];
 
-function NumbersButton({ className }: { className?: string }) {
+function NumbersButton() {
   return numbers.length
-    ? <BuySelectedNumbersButton className={className} />
-    : <CannotBuyButton className={className} />
+    ? <BuySelectedNumbersButton />
+    : <CannotBuyButton />
 }
 
 export default function Numbers() {
   return (
-    <div className="my-24 flex justify-between items-end">
-      <div className="flex-col">
-        <Title>Compre seu número por apenas <span className="text-blue-light">R$5,00</span></Title>
-        <NumbersButton className="my-12" />
+    <div className="my-24 items-end">
+      <Title className="!font-medium">Compre seu número por apenas <span className="text-blue-light">R$5,00</span></Title>
+      <div className="bg-gray-dark p-8 rounded-xl my-8">
+        <div>
+          <SmallText>Selecione os números a serem comprados*</SmallText>
+          <SmallText className="!font-medium">R$ 5,00/número</SmallText>
+        </div>
+        <div className="flex justify-between items-center">
+          <NumbersButton />
+          <SmallText className="!text-gray-light">*Selecione quantos números desejar</SmallText>
+        </div>
       </div>
     </div>
   )

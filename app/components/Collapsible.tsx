37c763b9cfbc255e842ca9c0b8aa959cdfdd27a9
rodrigocitadin <citadinrodrigo@gmail.com>
2023-12-30 11:51:22 -0300

    wip collapsible almost done

--- app/components/Collapsible.tsx
+++ app/components/Collapsible.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from "react";
+
 export default function Collapsible({ className, children, title }:
   {
     className?: string,
@@ -5,13 +9,20 @@ export default function Collapsible({ className, children, title }:
     title: string
   }
 ) {
-  const isHidden = true;
+  const [isHidden, setIsHidden] = useState(true);
+  const [isRotated, setIsRotated] = useState(false);
+  let rotate = isRotated ? "rotate-180" : "rotate-0";
+
+  function handleClick() {
+    setIsHidden(!isHidden);
+    setIsRotated(!isRotated);
+  }
 
   return (
     <div className={`${className} bg-gray-dark my-4 p-8 rounded-xl`}>
       <div className="flex justify-between items-center">
         <p className="text-blue-light text-2xl font-normal">{title}</p>
-        <button><img src="toggle.svg" /></button>
+        <button onClick={handleClick}><img className={rotate} src="toggle.svg" /></button>
       </div>
       <p className={`text-lg font-light mt-4 ${isHidden ? "hidden" : ""
         }`}>

ef7f3f9a4b5ba6b71878453f8d8d04e2298328d5
rodrigocitadin <citadinrodrigo@gmail.com>
2024-01-02 18:17:48 -0300

    change links

--- app/components/AsideNav.tsx
+++ app/components/AsideNav.tsx
@@ -17,7 +17,7 @@ export default function AsideNav() {
   return (
     <>
       <div className="flex justify-between mt-8 align-middle lg:hidden">
-        <a href='#'><img src='/logo.svg' /></a>
+        <a href='/'><img src='/logo.svg' /></a>
         <button onClick={showAside}>
           <img className="w-8" src="menu.svg" />
         </button>
@@ -26,13 +26,13 @@ export default function AsideNav() {
       <aside className={`z-10 top-0 w-screen right-0 fixed bg-gray-dark ${hide}`}>
         <nav className={`flex flex-col justify-between m-8`}>
           <div className="flex justify-between align-middle">
-            <a href='#'><img src='/logo.svg' /></a>
+            <a href='/'><img src='/logo.svg' /></a>
             <button onClick={hideAside}>
               <img src="close.svg" />
             </button>
           </div>
           <div className='flex flex-col'>
-            <a href='#' className="my-2 mt-8">Inicio</a>
+            <a href='/' className="my-2 mt-8">Inicio</a>
             <a href='#sobre' className="my-2">Como funciona?</a>
             <a href='#prêmio' className="my-2">Prêmio</a>
             <BuyNumberButton className="my-2 mt-8" />
--- app/components/FullNav.tsx
+++ app/components/FullNav.tsx
@@ -3,9 +3,9 @@ import BuyNumberButton from "./BuyNumberButton";
 export default function FullNav() {
   return (
     <nav className='hidden justify-between my-8 lg:flex'>
-      <a href='#'><img src='/logo.svg' /></a>
+      <a href='/'><img src='/logo.svg' /></a>
       <ul className='flex'>
-        <li className='mx-5 my-auto'><a href='#'>Inicio</a></li>
+        <li className='mx-5 my-auto'><a href='/'>Inicio</a></li>
         <li className='mx-5 my-auto'><a href='#sobre'>Como funciona?</a></li>
         <li className='mx-5 my-auto'><a href='#prêmio'>Prêmio</a></li>
         <li className='mx-5 my-auto'>
